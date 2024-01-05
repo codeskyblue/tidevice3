8d79f72f96d591ab51634748eb9d7e2b578b5867
codeskyblue <codeskyblue@gmail.com>
2024-01-05 19:51:04 +0800

    update global options

--- tidevice3/cli/cli_common.py
+++ tidevice3/cli/cli_common.py
@@ -4,34 +4,26 @@
 """Created on Fri Jan 05 2024 18:10:32 by codeskyblue
 """
 from __future__ import annotations
-from functools import lru_cache
 
 import click
 from dataclasses import dataclass
-from pymobiledevice3.cli.cli_common import USBMUX_OPTION_HELP, print_json
+from pymobiledevice3.cli.cli_common import USBMUX_OPTION_HELP
 from pymobiledevice3.lockdown import LockdownClient
 from pymobiledevice3.lockdown import create_using_usbmux
 
 
 @dataclass
 class GlobalConfig:
-    udid: str | None
-    usb: bool | None
-    network: bool | None
-    usbmux_address: str | None
+    udid: str | None = None
+    color: bool = True
+    usbmux_address: str | None = None
 
     def get_lockdown_client(self) -> LockdownClient:
-        connection_type = None
-        if self.usb:
-            connection_type = 'USB'
-        elif self.network:
-            connection_type = 'Network'
         return create_using_usbmux(serial=self.udid,
-                                   usbmux_address=self.usbmux_address,
-                                   connection_type=connection_type)
+                                   usbmux_address=self.usbmux_address)
 
 
-gcfg: GlobalConfig = GlobalConfig(None, None, None, None)
+gcfg: GlobalConfig = GlobalConfig()
 
 
 def get_udid() -> str:
@@ -40,15 +32,12 @@ def get_udid() -> str:
     raise RuntimeError('udid not set')
 
 
-# @click.option('--color/--no-color', default=True)
 @click.group()
 @click.option('-u', '--udid', default=None, help='udid of device')
-@click.option('-u', '--usb', is_flag=True, help='show only usb devices')
-@click.option('-n', '--network', is_flag=True, help='show only network devices')
 @click.option('usbmux_address', '--usbmux', help=USBMUX_OPTION_HELP)
-def cli(udid: str, usb: bool, network: bool, usbmux_address: str):
+@click.option('--color/--no-color', default=True)
+def cli(udid: str, usbmux_address: str, color: bool):
     gcfg.udid = udid
-    gcfg.usb = usb
-    gcfg.network = network
     gcfg.usbmux_address = usbmux_address
+    gcfg.color = color
     
--- tidevice3/cli/list.py
+++ tidevice3/cli/list.py
@@ -4,7 +4,7 @@
 """Created on Fri Jan 05 2024 18:10:01 by codeskyblue
 """
 import click
-from tidevice3.cli.cli_common import cli
+from tidevice3.cli.cli_common import cli, gcfg
 from pymobiledevice3.lockdown import usbmux
 from pymobiledevice3 import usbmux
 from pymobiledevice3.cli.cli_common import USBMUX_OPTION_HELP, print_json
@@ -13,14 +13,12 @@ from pymobiledevice3.tcp_forwarder import UsbmuxTcpForwarder
 
 
 @cli.command()
-@click.option('usbmux_address', '--usbmux', help=USBMUX_OPTION_HELP)
-@click.option('--color/--no-color', default=True)
 @click.option('-u', '--usb', is_flag=True, help='show only usb devices')
 @click.option('-n', '--network', is_flag=True, help='show only network devices')
-def list(usbmux_address: str, color: bool, usb: bool, network: bool):
+def list(usb: bool, network: bool):
     """ list connected devices """
     connected_devices = []
-    for device in usbmux.list_devices(usbmux_address=usbmux_address):
+    for device in usbmux.list_devices(usbmux_address=gcfg.usbmux_address):
         udid = device.serial
 
         if usb and not device.is_usb:
@@ -30,8 +28,8 @@ def list(usbmux_address: str, color: bool, usb: bool, network: bool):
             continue
 
         lockdown = create_using_usbmux(udid, autopair=False, connection_type=device.connection_type,
-                                       usbmux_address=usbmux_address)
+                                       usbmux_address=gcfg.usbmux_address)
         connected_devices.append(lockdown.short_info)
 
-    print_json(connected_devices, colored=color)
+    print_json(connected_devices, colored=gcfg.color)
 
